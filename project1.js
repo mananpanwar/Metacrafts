13c4f57d939832932c57cb2390db0cb6f5e76dca
Manan Panwar <manancoder12213@gmail.com>
2024-06-02 23:53:58 +0530

    Update project1.js

--- project1.js
+++ project1.js
@@ -1,1 +1,25 @@
+var nftCollection = [];
 
+mintNFT = (nftName, nftId, nftDescription) => {
+    const nftObject = {
+        name: nftName,
+        nftId: nftId,
+        detail: nftDescription
+    };
+
+    nftCollection.push(nftObject);
+};
+
+listNFTs = () => {
+    nftCollection.forEach((nft, index) => {
+        console.log(`NFT #${index + 1}`);
+        console.log(`Name: ${nft.name}`);
+        console.log(`NtfId: ${nft.nftId}`);
+        console.log(`Details: ${nft.detail}`);
+    });
+};
+
+getTotalSupply = () => {
+    const count = nftCollection.length;
+    console.log(`Total no. of Nft's are ${count}`);
+};
